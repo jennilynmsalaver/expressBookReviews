d5d8c7dcb4a1b4f4e111a01575d3dec11df4f4e3
jennilynmsalaver <jennilyn.m.salaver@gmail.com>
2025-11-10 23:47:23 +0800

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,10 +11,28 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+ const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(403).json({ message: "Access forbidden: No token provided" });
+    }
+
+    jwt.verify(token, "access_token_secret", (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Invalid or expired token" });
+        }
+
+        req.username = decoded.username;
+        next();
+    });
 });
- 
-const PORT =5000;
+
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
